b53e5af4060c2c820feb62dc66762a1f5693d16b
SpeedRacer <robertgeorge2@gmail.com>
2021-07-10 16:58:01 -0400

    fixed ESLint conflicts

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,32 +1,32 @@
-const router = require("express").Router();
-const ensureAuthenticated = require("../middlewares/ensureAuthenticated");
+const router = require('express').Router();
+const ensureAuthenticated = require('../middlewares/ensureAuthenticated');
 
 module.exports = (passport, db) => {
-	const AuthController = require("../controllers/authController")(passport, db);
-	const AppController = require("../controllers/appController")(db);
+  const AuthController = require('../controllers/authController')(passport, db);
+  const AppController = require('../controllers/appController')(db);
 
-	// Authentication
-	router.post("/register", AuthController.register);
-	router.post("/login", AuthController.login);
-	router.get("/logout", AuthController.logout);
-	router.put("/user/:id", ensureAuthenticated, AuthController.updateUser);
-	router.delete("/user/:id", ensureAuthenticated, AuthController.deleteUser);
-	router.post("/user/confirm", AuthController.confirmAuth);
+  // Authentication
+  router.post('/register', AuthController.register);
+  router.post('/login', AuthController.login);
+  router.get('/logout', AuthController.logout);
+  router.put('/user/:id', ensureAuthenticated, AuthController.updateUser);
+  router.delete('/user/:id', ensureAuthenticated, AuthController.deleteUser);
+  router.post('/user/confirm', AuthController.confirmAuth);
 
-	// Trivia App
-	router.get("/trivia", AppController.getExamples);
-	router.post("/trivia", AppController.createExample);
-	router.delete("/trivia/:id", AppController.deleteExample);
+  // Trivia App
+  router.get('/trivia', AppController.getExamples);
+  router.post('/trivia', AppController.createExample);
+  router.delete('/trivia/:id', AppController.deleteExample);
 
-	// Hangman App
-	router.get("/hangman", AppController.getExamples);
-	router.post("/hangman", AppController.createExample);
-	router.delete("/hangman/:id", AppController.deleteExample);
+  // Hangman App
+  router.get('/hangman', AppController.getExamples);
+  router.post('/hangman', AppController.createExample);
+  router.delete('/hangman/:id', AppController.deleteExample);
 
-	// App
-	router.get("/examples", AppController.getExamples);
-	router.post("/examples", AppController.createExample);
-	router.delete("/examples/:id", AppController.deleteExample);
+  // App
+  router.get('/examples', AppController.getExamples);
+  router.post('/examples', AppController.createExample);
+  router.delete('/examples/:id', AppController.deleteExample);
 
-	return router;
+  return router;
 };
