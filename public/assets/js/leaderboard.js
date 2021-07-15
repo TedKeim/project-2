9403abb30a38d9ab527b7d7f406148e3f188d728
SpeedRacer <robertgeorge2@gmail.com>
2021-07-15 17:37:35 -0400

    Made udates to get Leaderboard working but snot finished yet.

--- public/assets/js/leaderboard.js
+++ public/assets/js/leaderboard.js
@@ -7,12 +7,39 @@ highScoresList.innerHTML = highScores
   })
   .join('');
 
-// The API object contains methods for each kind of request we'll make
-const API = {
-  getLeaderboards: function () {
-    return $.ajax({
-      url: 'api/leaderboards',
-      type: 'GET'
-    });
-  }
-};
+// // Get references to page elements
+// const $userScore = $('#user-score');
+// const $user = $('#user');
+// const $scoreList = $('#score-list');
+
+// // The API object contains methods for each kind of request we'll make
+// const API = {
+//   getHangmanScores: function () {
+//     return $.ajax({
+//       url: 'api/hangman',
+//       type: 'GET'
+//     });
+//   }
+// };
+
+// // refreshExamples gets new examples from the db and repopulates the list
+// // const refreshHangmanScores = function () {
+// API.getHangmanScores().then(function (data) {
+//   const $scores = data.map(function (score) {
+//     const $span = $('<span>').text($userScore.text);
+//     // .attr('href', '/example/' + example.id);
+
+//     const $li = $('<li>')
+//       .attr({
+//         class: 'list-group-item',
+//         'data-id': $userScore.id
+//       })
+//       .append($span);
+
+//     return $li;
+//   });
+
+//   $scoreList.empty();
+//   $scoreList.append($scores);
+// });
+// // };
