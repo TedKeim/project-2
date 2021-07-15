bd97b91766976a85e841006c97121009e9dfdda9
Tcomp <williamt7786@gmail.com>
2021-07-15 14:37:49 -0400

    for api

---
+++ public/assets/js/Jokesapi.js
@@ -0,0 +1,29 @@
+
+// const settings = {
+// 	"async": true,
+// 	"crossDomain": true,
+// 	"url": "https://geek-jokes.p.rapidapi.com/api?format=json",
+// 	"method": "GET",
+// 	"headers": {
+// 		"x-rapidapi-key": "401018446bmsh98223740afd4f69p1ee68djsn7e81cb95e5fc",
+// 		"x-rapidapi-host": "geek-jokes.p.rapidapi.com"
+// 	}
+// };
+
+// $.ajax(settings).done(function (response) {
+// 	console.log(response);
+// });
+const settings = {
+	"async": true,
+	"crossDomain": true,
+	"url": "https://jokeapi-v2.p.rapidapi.com/joke/Any?format=json&contains=C%2523&idRange=0-150&blacklistFlags=nsfw%2Cracist",
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-key": "401018446bmsh98223740afd4f69p1ee68djsn7e81cb95e5fc",
+		"x-rapidapi-host": "jokeapi-v2.p.rapidapi.com"
+	}
+};
+
+$.ajax(settings).done(function (response) {
+	console.log(response);
+});
